107399b1730944187b7586019dae0bed58524245
ogsilvaa <ogsilvaa@gmail.com>
2014-03-19 23:20:18 -0500

    test con DataTable

--- Test/MainTest.cs
+++ Test/MainTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MTech.Cdg.Core;
+using System.Data;
 
 namespace Test
 {
@@ -29,21 +30,28 @@ namespace Test
                 Columns = new List<Column> { cNombre, cApellido }
             };
 
+            var dt = new DataTable("Persona");
+            
+
             var pks = new Dictionary<string, List<string>>();
             tPersona.Columns.Where(x=>x.IsPk).ToList().ForEach(x => pks.Add(x.Name, new List<string>()));
+            tPersona.Columns.ForEach(x => dt.Columns.Add(x.Name));
 
             const int count = 100;
             var results = new string[count, tPersona.Columns.Count];
             for (var i = 0; i < count; i++)
             {
+                var row = dt.NewRow();
                 foreach (var col in tPersona.Columns)
                 {
                     var gen = GenerarString(col.size);
                     results[i, tPersona.Columns.IndexOf(col)] = gen;
-
+                    row[col.Name] = gen;
                     if (col.IsPk) pks[col.Name].Add(gen);
                 }
+                dt.Rows.Add(row);
             }
+
             Assert.AreEqual(results.Length, 200);
 
         }
